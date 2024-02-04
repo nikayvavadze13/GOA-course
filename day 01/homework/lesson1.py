e09c2b2bd0ab30826599fc3414b44e81c018c82f
nikayvavadze13 <nika.yvavadze4@gmail.com>
2024-02-04 21:27:30 +0400

    first commit

---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,7 @@
+name = "my name is nika"
+surname = "my surname is yvavadze"
+height = "i am 1,72 in meters"
+story = "i joined GOA because i want to improve and upgrade myself"
+age = "my age is 14"
+
+print(name + " " + surname + " " + height + " " + age + " " + story + "." )
