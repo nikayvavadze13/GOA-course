2d2ddb4b4d3f8d5764e31b3d5549ba88ac6e4d57
nikayvavadze13 <nika.yvavadze4@gmail.com>
2024-02-25 00:35:21 +0400

    fifth commit

---
+++ day 07/homework/lesson 07.py
@@ -0,0 +1,26 @@
+#delcaring my hobby age and my name
+my_name = "nika"
+my_age = 14
+my_hobby =  "coding"  
+
+#declaring my sister's hobby age and name
+sister_name = "ani"
+sister_age = 10.5
+sister_hobby = "watching youtube"
+
+print(my_age > sister_age) #boolean type (true)
+print(my_age < sister_age) #boolean type (false)
+print(my_age >= sister_age) #boolean type (true)
+print(my_age <= sister_age) #boolean type (false)
+print(my_name + " " + "is" + " " + str(my_age) + "yo") #using function str
+print(sister_name + " " + "hobby" + " " + "is" + " " + str(sister_hobby)) #again using int function
+print(int(sister_age)) #using int function
+print(int(my_age)) #again using int function
+print(float(sister_age)) #using float function
+print(float(my_age)) #using float function
+print(my_age) #just printing declared variable
+print(sister_age) #just printing declared variable
+print("i" + " " + "am" + " " + my_name) #just string type sentence
+print("my sister's" + " " + "name" + " " + "is" + " " + sister_name)#just string type sentence of my sister
+print("i" + " " + "am" + " " + str(my_name) + "," + "i" + " " + "am" + " " + str(my_age) + "yo" + " " + "and" +
+       " " + "my" + " " + "hobby" + " " + "is" + " " + str(my_hobby)) #printed sentence about me using str function
---
+++ day 08/homework/lesson 08.py
@@ -0,0 +1,19 @@
+#printing from 1 up to 100 using for loop
+for i in range(100):
+    print(i)
+#printing from 1 up to 100 using while loop
+num = 0
+while():
+   num+=1
+   print(num)
+   if(num==100):
+       break
+#first 10 number 
+multiply = 0
+for i in range(10):
+    multiply += i
+    print(multiply)
+#printing even numbers under 20
+for i in range(0, 21, 2):
+    print(i)
+
