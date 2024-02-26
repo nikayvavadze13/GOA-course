ce91522051ea88b728d74c090eab09b4632c98f3
nikayvavadze13 <nika.yvavadze4@gmail.com>
2024-02-26 21:19:00 +0400

    first commit

---
+++ day 09/homework/lesson 09.py
@@ -0,0 +1,4 @@
+for i in range(1, 101, 2):
+     print(str(i) + "კენტია")
+     print(str(i + 1) + "ლუწია")
+
---
+++ day 10/homework/lesson 10.py
@@ -0,0 +1,15 @@
+password = "123"
+authorised = True
+
+while authorised != password:
+    user_input = input("enter your password")
+    if user_input == password:
+         print("acces granted")
+         authorised = True
+
+
+
+
+
+
+    
