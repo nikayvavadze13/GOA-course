64d17a1f818f2ea36ed15ad81c54869e177f3731
nikayvavadze13 <nika.yvavadze4@gmail.com>
2024-03-06 20:56:51 +0400

    first commit

---
+++ day 12/homework/lesson 12(1).py
@@ -0,0 +1,7 @@
+num = 1
+sum = 0
+while num <= 10:
+    print(num)
+    sum += num
+    num += 1
+print(sum)
---
+++ day 12/homework/lesson12 (4).py
@@ -0,0 +1,9 @@
+# first way to make homework
+for i in range(6, 20, 6):
+    print(i)
+
+
+# second way to make homework
+for u in range(1, 20):
+    if u % 6 == 0:
+        print(u)
